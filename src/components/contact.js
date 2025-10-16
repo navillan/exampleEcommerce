383012285d7b05a60315e96f191256e694a31840
Mert <espawnn@gmail.com>
2025-10-16 16:28:57 +0300

    Project v0.2

---
+++ src/components/contact.js
@@ -0,0 +1,12 @@
+
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>If you have any questions, please reach out to us at <a href="mailto:support@example.com">support@example.com</a>.</p> 
+    </div>
+  );
+}
+
+export default Contact;
