507bfbd994396d9cf3ee9b4dd8cc42875eff70c2
Mert <espawnn@gmail.com>
2025-10-16 16:32:03 +0300

    Project v0.2

---
+++ src/components/aboutUs.js
@@ -0,0 +1,14 @@
+
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our e-commerce platform! We are dedicated to providing you with the best online shopping experience.</p>
+      <p>Our mission is to offer a wide range of products at competitive prices, along with exceptional customer service.</p>
+      <p>Thank you for choosing us for your shopping needs!</p>
+    </div>
+  );
+}
+
+export default AboutUs;
