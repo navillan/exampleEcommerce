3c501b2428db79e2b9707208998a17ad7cf8795a
Mert <espawnn@gmail.com>
2025-10-28 17:52:30 +0300

    Project v0.6

--- src/components/content.js
+++ src/components/content.js
@@ -1,5 +1,5 @@
 import { useState, useMemo, useEffect } from "react";
-import { useUpdateCart } from "../hooks/useMyCart.js";
+import { useUpdateCart } from "../utils/updateMyCart.js";
 import $ from "jquery";
 
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App.js';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App.js";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const basename = (() => {
+  try {
+    const url = new URL(process.env.PUBLIC_URL || "/", window.location.origin);
+    const path = url.pathname;
+    return path.endsWith("/") ? (path.length > 1 ? path.slice(0, -1) : "/") : path;
+  } catch {
+    return "/";
+  }
+})();
+
 root.render(
-  <BrowserRouter basename="/">
-   <App />
+  <BrowserRouter basename={basename}>
+    <App />
   </BrowserRouter>
 );
